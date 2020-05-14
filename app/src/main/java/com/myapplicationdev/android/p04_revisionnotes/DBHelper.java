0c907490b91a438d1df5acc0f9d14c2b2f2cc088
KarenKuan <18055889@myrp.edu.sg>
2020-05-14 23:04:13 +0800

    P04-Mini Problem Statement

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -12,18 +12,30 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
 	//TODO Define the Database properties
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = ;
+	// Filename of the database
+	private static final String DATABASE_NAME = "revisionnote.db";
+	// Start version with 1
+	// increment by 1 whatever db schema changes.
+	private static final int DATABASE_VERSION = 1;
 
+	private static final String TABLE_NOTE = "Note";
+	private static final String COLUMN_ID = "_id";
+	private static final String COLUMN_NOTE_CONTENT = "note_content";
+	private static final String COLUMN_STARS = "stars";
 
 	public DBHelper(Context context) {
-
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		//TODO CREATE TABLE Note
+		String createNoteTableSql = "CREATE TABLE " + TABLE_NOTE + "("
+				+ COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ COLUMN_NOTE_CONTENT + " TEXT,"
+				+ COLUMN_STARS + " INTEGER )";
+		db.execSQL(createNoteTableSql);
+		Log.i("info", "created tables");
 
 	}
 
@@ -35,10 +47,59 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	public void insertNote(String noteContent, int stars) {
 		//TODO insert the data into the database
+		// Get an instance of the database for writing
+		SQLiteDatabase db = this.getWritableDatabase();
+		// We use ContentValues object to store the values for
+		// the db operation
+		ContentValues values = new ContentValues();
+		// Store the column name as key and the description as value
+		values.put(COLUMN_NOTE_CONTENT, noteContent);
+		// Store the column name as key and the date as value
+		values.put(COLUMN_STARS, stars);
+		// Insert the row into the TABLE_TASK
+		db.insert(TABLE_NOTE, null, values);
+		// Close the database connection
+		db.close();
 	}
 
 	public ArrayList<Note> getAllNotes() {
 		//TODO return records in Java objects
+		// Create an ArrayList that holds String objects
+		ArrayList<Note> notes = new ArrayList<Note>();
+		// Select all the tasks' description
+		String selectQuery = "SELECT " + COLUMN_ID + ", "
+				+ COLUMN_NOTE_CONTENT + ", "
+				+ COLUMN_STARS
+				+ " FROM " + TABLE_NOTE;
+
+		// Get the instance of database to read
+		SQLiteDatabase db = this.getReadableDatabase();
+		// Run the SQL query and get back the Cursor object
+		Cursor cursor = db.rawQuery(selectQuery, null);
+
+		// moveToFirst() moves to first row, null if no records
+		if (cursor.moveToFirst()) {
+			// Loop while moveToNext() points to next row
+			//  and returns true; moveToNext() returns false
+			//  when no more next row to move to
+			do {
+				// Add the task content to the ArrayList object
+				//  getString(0) retrieves first column data
+				//  getString(1) return second column data
+				//  getInt(0) if data is an integer value
+				int id = cursor.getInt(0);
+				String title = cursor.getString(1);
+				int stars = cursor.getInt(2);
+
+				Note note = new Note(id, title, stars);
+				notes.add(note);
+			} while (cursor.moveToNext());
+		}
+		// Close connection
+		cursor.close();
+		db.close();
+
+		return notes;
 	}
 
     public ArrayList<String> getNoteContent() {
@@ -47,7 +108,8 @@ public class DBHelper extends SQLiteOpenHelper {
 		// Create an ArrayList that holds String objects
         ArrayList<String> notes = new ArrayList<String>();
         // Select all the notes' content
-        String selectQuery = "";
+        String selectQuery = "SELECT " + COLUMN_NOTE_CONTENT
+				+ " FROM " + TABLE_NOTE;
 
         // Get the instance of database to read
         SQLiteDatabase db = this.getReadableDatabase();
@@ -58,9 +120,12 @@ public class DBHelper extends SQLiteOpenHelper {
             // Loop while moveToNext() points to next row and returns true;
             // moveToNext() returns false when no more next row to move to
             do {
-
-
-            } while (cursor.moveToNext());
+				// Add the task content to the ArrayList object
+				//  getString(0) retrieves first column data
+				//  getString(1) return second column data
+				//  getInt(0) if data is an integer value
+				notes.add(cursor.getString(0));
+			} while (cursor.moveToNext());
         }
         // Close connection
         cursor.close();
--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -2,14 +2,71 @@ package com.myapplicationdev.android.p04_revisionnotes;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNote;
+    TextView tvViewNote, tvViewStars;
+    RadioGroup rg;
+    RadioButton rbStars;
+    Button btnInsert, btnShowList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNote = findViewById(R.id.editTextNote);
+        tvViewNote = findViewById(R.id.textViewNote);
+        tvViewStars = findViewById(R.id.textView2);
+        rg = findViewById(R.id.radioGroupStars);
+        rbStars = findViewById(R.id.radio1);
+        rbStars = findViewById(R.id.radio2);
+        rbStars = findViewById(R.id.radio3);
+        rbStars = findViewById(R.id.radio4);
+        rbStars = findViewById(R.id.radio5);
+        btnInsert = findViewById(R.id.buttonInsertNote);
+        btnShowList = findViewById(R.id.buttonShowList);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                String text = etNote.getText().toString();
+                Toast.makeText(MainActivity.this, "Inserted",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnShowList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getNoteContent();
+                db.close();
+
+
+
+            }
+        });
     }
 }
